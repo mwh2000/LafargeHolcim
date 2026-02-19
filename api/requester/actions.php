57e7bf03a71a8f387e19e9f28ff1dab0fc378c6e
Mustafa <asasimr55@gmail.com>
2026-02-19 23:03:02 +0300

    fix: remove unnecessary CORS headers from API response files

--- api/requester/actions.php
+++ api/requester/actions.php
@@ -1,8 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
 
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../../core/Database.php';
